0f0b3c39d9163ce6520db6491b08fa3478d6f457
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-17 14:25:17 -0800

    ♻️  Refactor cli expects iso date format

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test-cli.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test-cli.R
@@ -2,7 +2,7 @@ describe("Test cli for write_type_of_traps", {
   it("Creates csv with type of traps", {
     data_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/data_tests/traps_daily_status_guadalupe.csv"
     output_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/number_of_traps_by_type.csv"
-    date <- "31/Ene/2025"
+    date <- "2025-01-31"
     options <- list("data_path" = data_path, "initial_date" = date, "output_path" = output_path)
     testtools::if_exist_remove(output_path)
     write_type_of_traps(options)
