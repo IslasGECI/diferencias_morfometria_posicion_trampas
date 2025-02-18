47a1475bfa876d0d2432d95c85dcb2244f9c3b3a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-18 09:56:24 -0800

    ♻️  Refactor

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test-count-type-of-traps.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test-count-type-of-traps.R
@@ -21,7 +21,7 @@ describe("🪙 Count active traps by type on certain period", {
     expected_number_of_rows <- 2
     obtained_number_of_rows <- nrow(obtained)
     expect_equal(obtained_number_of_rows, expected_number_of_rows)
-    expect_true(is.Date(obtained$Fecha))
+    expect_true(lubridate::is.Date(obtained$Fecha))
   })
 })
 describe("Count active traps by type", {
