0c8908cf2be2a513c082df042c679a3481eb7376
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-19 11:55:14 -0800

    🛑🧪 Fail tests

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test-cli.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test-cli.R
@@ -1,18 +1,28 @@
-describe("Cli for transform UTM to latlon", {
-  it("convert_to_latlon()", {
-    utm_coordinates_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/data_tests/morfometria_capitalized.csv"
-    utm_coordinates <- readr::read_csv(utm_coordinates_path, show_col_types = FALSE)
-    output_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/ids_in_latlon_socorro.csv"
-    options <- list("data_path" = utm_coordinates_path, "output_path" = output_path)
-    testtools::if_exist_remove(output_path)
-    convert_to_latlon_socorro(options)
+describe("Cli for transform UTM to latlon to Socorro", {
+  utm_coordinates_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/data_tests/morphometry_socorro.csv"
+  utm_coordinates <- readr::read_csv(utm_coordinates_path, show_col_types = FALSE)
+  output_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/ids_in_latlon_socorro.csv"
+  options <- list("data_path" = utm_coordinates_path, "output_path" = output_path)
+  testtools::if_exist_remove(output_path)
+  convert_to_latlon_socorro(options)
+  it("convert_to_latlon_socorro()", {
     expect_true(testtools::exist_output_file(output_path))
-
     obtained_data <- readr::read_csv(output_path, show_col_types = FALSE)
     added_columns <- c("Coordenada_Este", "Coordenada_Norte")
     expect_true(all(!added_columns %in% colnames(obtained_data)))
-    # testtools::if_exist_remove(output_path)
   })
+  it("check coordinates range", {
+    max_lat <- 18.8599
+    min_lat <- 18.7218
+    max_lon <- -110.9173
+    min_lon <- -111.0536
+    obtained_data <- readr::read_csv(output_path, show_col_types = FALSE)
+    expect_true(all(obtained_data$latitude < max_lat))
+    expect_true(all(obtained_data$latitude > min_lat))
+    expect_true(all(obtained_data$longitude < max_lon))
+    expect_true(all(obtained_data$longitude > min_lon))
+  })
+  # testtools::if_exist_remove(output_path)
 })
 
 describe("Test cli for write_type_of_traps", {
