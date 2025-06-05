1cd50acc5165eaa17d5ce7e8c59ba78f7deff56d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-05 11:50:37 -0700

    ♻️  Refactor move clis to cli file

--- diferenciasMorfometriaPosicionTrampas/R/clean_morphometry.R
+++ diferenciasMorfometriaPosicionTrampas/R/clean_morphometry.R
@@ -1,21 +1,3 @@
-#' @export
-write_cleaned_morphometry <- function(options) {
-  output_file <- options$output_path
-  morphometry <- .read_morphometry_file(options)
-  morphometry |>
-    .clean_morphometry() |>
-    readr::write_csv(output_file)
-}
-
-#' @export
-write_cleaned_socorro_morphometry <- function(options) {
-  output_file <- options$output_path
-  morphometry <- .read_morphometry_file(options)
-  morphometry |>
-    .clean_morphometry(zone = 12) |>
-    readr::write_csv(output_file)
-}
-
 .read_morphometry_file <- function(options) {
   input_file <- options$data_path
   morphometry <- readr::read_csv(input_file, show_col_types = FALSE)
--- diferenciasMorfometriaPosicionTrampas/R/cli.R
+++ diferenciasMorfometriaPosicionTrampas/R/cli.R
@@ -30,3 +30,21 @@ write_type_of_traps <- function(options) {
     count_active_traps_after_date(options$initial_date) |>
     readr::write_csv(output_file)
 }
+
+#' @export
+write_cleaned_morphometry <- function(options) {
+  output_file <- options$output_path
+  morphometry <- .read_morphometry_file(options)
+  morphometry |>
+    .clean_morphometry() |>
+    readr::write_csv(output_file)
+}
+
+#' @export
+write_cleaned_socorro_morphometry <- function(options) {
+  output_file <- options$output_path
+  morphometry <- .read_morphometry_file(options)
+  morphometry |>
+    .clean_morphometry(zone = 12) |>
+    readr::write_csv(output_file)
+}
