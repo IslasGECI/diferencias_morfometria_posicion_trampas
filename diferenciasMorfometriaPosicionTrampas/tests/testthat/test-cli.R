151d8f45df202195a1beb244a62af1b57e7c02e5
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-05 11:45:52 -0700

    🛑🧪 Fail tests

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test-cli.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test-cli.R
@@ -43,12 +43,8 @@ describe("Test cli for write_type_of_traps", {
     date <- "2025-02-01"
     options <- list("data_path" = data_path, "initial_date" = date, "output_path" = output_path)
     testtools::if_exist_remove(output_path)
-    write_type_of_traps(options)
+    expect_warning(write_type_of_traps(options))
     expect_true(testtools::exist_output_file(output_path))
-    expect_warning(
-      write_type_of_traps(options),
-      "The count is not accurate for unique positions ID. Use `write_type_of_traps_by_id()` instead."
-    )
     testtools::if_exist_remove(output_path)
   })
 })
