ddafc859cb3c987bf3ed4a83abf9be1dd16439d4
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2025-06-05 10:06:15 -0700

    Revert "🛑🧪 Fail tests"
    This reverts commit 473befc98fe52f695e16766817d8f974e203e61e.

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test-adapters.R
+++
@@ -1,6 +0,0 @@
-describe("Adapter for splited trap daily status", {
-  data_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/data_tests/traps_daily_status_guadalupe.csv"
-  it("split id", {
-    obtained <- split_id(data_path)
-  })
-})
