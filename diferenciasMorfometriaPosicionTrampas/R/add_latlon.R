0f63e4b2172f796bc53c3377c4b6a2d852dcc622
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-28 10:45:25 -0700

    ♻️  Refactor

--- diferenciasMorfometriaPosicionTrampas/R/add_latlon.R
+++ diferenciasMorfometriaPosicionTrampas/R/add_latlon.R
@@ -1,20 +1,3 @@
-#' @export
-convert_to_latlon_socorro <- function(options) {
-  utm_data <- readr::read_csv(options[["data_path"]], show_col_types = FALSE)
-  utm_data |>
-    add_latlon(zone = 12) |>
-    readr::write_csv(options[["output_path"]])
-}
-#' @export
-convert_to_latlon <- function(options) {
-  utm_data <- readr::read_csv(options[["data_path"]], show_col_types = FALSE)
-  utm_data |>
-    add_latlon() |>
-    dplyr::select(-c("Coordenada_Este", "Coordenada_Norte")) |>
-    readr::write_csv(options[["output_path"]])
-}
-
-
 add_latlon <- function(utm_coordinates, zone = 11) {
   lonlat <- oce::utm2lonlat(utm_coordinates$Coordenada_Este, utm_coordinates$Coordenada_Norte, zone = zone)
   utm_coordinates |> dplyr::mutate("latitude" = lonlat$latitude, "longitude" = lonlat$longitude)
--- diferenciasMorfometriaPosicionTrampas/R/cli.R
+++ diferenciasMorfometriaPosicionTrampas/R/cli.R
@@ -25,3 +25,20 @@ write_cleaned_socorro_morphometry <- function(options) {
     .clean_morphometry(zone = 12) |>
     readr::write_csv(output_file)
 }
+
+#' @export
+convert_to_latlon_socorro <- function(options) {
+  utm_data <- readr::read_csv(options[["data_path"]], show_col_types = FALSE)
+  utm_data |>
+    add_latlon(zone = 12) |>
+    readr::write_csv(options[["output_path"]])
+}
+
+#' @export
+convert_to_latlon <- function(options) {
+  utm_data <- readr::read_csv(options[["data_path"]], show_col_types = FALSE)
+  utm_data |>
+    add_latlon() |>
+    dplyr::select(-c("Coordenada_Este", "Coordenada_Norte")) |>
+    readr::write_csv(options[["output_path"]])
+}
