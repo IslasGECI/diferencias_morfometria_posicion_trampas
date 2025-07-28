b648c5aed83cb920aee25a498959ea0e110787b8
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-28 10:35:11 -0700

    ✅ Pass tests

--- diferenciasMorfometriaPosicionTrampas/R/add_latlon.R
+++ diferenciasMorfometriaPosicionTrampas/R/add_latlon.R
@@ -3,7 +3,6 @@ convert_to_latlon_socorro <- function(options) {
   utm_data <- readr::read_csv(options[["data_path"]], show_col_types = FALSE)
   utm_data |>
     add_latlon(zone = 12) |>
-    dplyr::select(-c("Coordenada_Este", "Coordenada_Norte")) |>
     readr::write_csv(options[["output_path"]])
 }
 #' @export
