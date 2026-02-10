a18ca83808bf3711cfda5b1ef6f0bd0ea6367c1d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-02-10 09:48:05 -0800

    🛑🧪 Fail tests

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test_get_domain_specific_options.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test_get_domain_specific_options.R
@@ -1,5 +1,5 @@
 describe("Get domain specific language options", {
   obtained_options <- get_domain_specific_options()
   expected_options <- c("trap-positions-path", "output-path")
-  expect_true(all(expected_options %in% obtained_options))
+  expect_true(all(expected_options %in% names(obtained_options)))
 })
