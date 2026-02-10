ca9b66117fc378afd7038f76e5d0a8d51bd1cd36
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-02-10 08:33:30 -0800

    🛑🧪 Fail tests

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test_effort_2_tidy.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test_effort_2_tidy.R
@@ -4,9 +4,9 @@ datos_prueba_sin_capturas <- readr::read_csv("/workdir/diferenciasMorfometriaPos
 
 describe("Que la función pase al formato tidy", {
   it("expected number of columns", {
-    obtained <- effort_2_tidy(datos_prueba)
+    obtained <- xxeffort_2_tidy(datos_prueba)
     obtained_columns <- colnames(obtained)
-    expected_columns <- c("ID_de_trampa", "Estado_trampa", "Fecha")
+    expected_columns <- c("ID_de_trampa", "Estado_trampa", "Fecha", "Atrayente")
     expect_equal(obtained_columns, expected_columns)
   })
   it("expected number of rows", {
