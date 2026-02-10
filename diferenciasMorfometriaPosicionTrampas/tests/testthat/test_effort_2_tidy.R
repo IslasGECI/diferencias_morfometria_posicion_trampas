4d4b9b6bffb09b8cb8b3d003b70d8d9acb8a3fdd
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-02-10 08:30:14 -0800

    ♻️  Refactor

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test_effort_2_tidy.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test_effort_2_tidy.R
@@ -1,9 +1,6 @@
-archivo_prueba <- "../data_tests/esfuerzo_data_tests.csv"
-datos_prueba <- readr::read_csv(archivo_prueba, show_col_types = FALSE)
-archivo_prueba <- "../data_tests/esfuerzo_data_tests_2.csv"
-datos_prueba_2 <- readr::read_csv(archivo_prueba, show_col_types = FALSE)
-archivo_prueba <- "../data_tests/esfuerzo_data_tests_sin_capturas.csv"
-datos_prueba_sin_capturas <- readr::read_csv(archivo_prueba, show_col_types = FALSE)
+datos_prueba <- readr::read_csv("/workdir/diferenciasMorfometriaPosicionTrampas/tests/data_tests/esfuerzo_data_tests.csv", show_col_types = FALSE)
+datos_prueba_2 <- readr::read_csv("/workdir/diferenciasMorfometriaPosicionTrampas/tests/data_tests/esfuerzo_data_tests_2.csv", show_col_types = FALSE)
+datos_prueba_sin_capturas <- readr::read_csv("/workdir/diferenciasMorfometriaPosicionTrampas/tests/data_tests/esfuerzo_data_tests_sin_capturas.csv", show_col_types = FALSE)
 
 describe("Que la función pase al formato tidy", {
   it("expected number of columns", {
