01d279f7514e384942e11c348fa0e74513e51c7d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-11 12:44:37 -0700

    ♻️  Refactor

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test_effort_2_tidy.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test_effort_2_tidy.R
@@ -16,14 +16,12 @@ describe("Que la función pase al formato tidy", {
     expect_equal(obtained_rows, expected_rows)
   })
   it("expected second output", {
-    salida_test_2 <- tibble::tibble(
-      ID_de_trampa = rep(c("TC-20-1084", "TC-20-1085", "TC-10-0980-MV"), each = 7),
-      Estado_trampa = c("A", "A", "X", "A", "A", "A", "A", "A", "A", "X", "A", "A", "A", "A", "X", "A", "A", "A", "A", "D", "D"),
-      Fecha = rep(c("4/27/2020", "4/28/2020", "4/29/2020", "4/30/2020", "5/1/2020", "5/2/2020", "5/3/2020"), 3),
-      Atrayente = c(rep("Cebo tradicional", 14), rep("Orina de lince", 7))
-    )
-    obtained <- effort_2_tidy(datos_prueba_2)
-    expect_equal(obtained, salida_test_2)
+    obtained <- xxeffort_2_tidy(datos_prueba_2)
+    obtained_rows <- nrow(obtained)
+    number_of_days <- 7
+    number_of_traps <- nrow(datos_prueba_2)
+    expected_rows <- number_of_days * number_of_traps
+    expect_equal(obtained_rows, expected_rows)
   })
   it("expected output without captures", {
     expect_message(effort_2_tidy(datos_prueba_sin_capturas), "NO HAY CAPTURAS")
