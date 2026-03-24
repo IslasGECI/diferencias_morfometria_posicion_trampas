80fed5bfff7684539cc3de7982200d6ace8f88cf
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-24 08:50:36 -0700

    🛑🧪 Fail tests

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test-get_captures.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test-get_captures.R
@@ -1,5 +1,5 @@
 describe("Filter captures", {
-  trap_status <- readr::read_csv("/workdir/diferenciasMorfometriaPosicionTrampas/tests/data_tests/splited_trap_daily_status.csv", show_col_types = FALSE)
+  trap_status <- readr::read_csv("/workdir/diferenciasMorfometriaPosicionTrampas/tests/data_tests/position_tidy_ISO.csv", show_col_types = FALSE)
   it("Filter captures as X", {
     obtained <- filter_captures(trap_status)
     expected_n_captures <- 3
