1a62d928b3568e0fd0387cece5cf7ac277da0f69
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-20 19:58:10 +0000

    ♻️  Refactor

--- src/clean_position_morphometry.sh
+++ src/clean_position_morphometry.sh
@@ -16,6 +16,13 @@ xlsx_2_csv(){
     cut_csv_file ${number_columns} ${tmp_file_name} ${csv_file_name}
 }
 
+xx_xlsx_2_csv(){
+    data_file=${1}
+    file_name=$(get_base_filename ${data_file})
+    tmp_file_name=${file_name}.tmp
+    in2csv --no-header-row --blanks ${data_file} | tail --lines=+2 > ${tmp_file_name}
+}
+
 get_base_filename(){
     data_file=${1}
     local src_path=$(dirname "${data_file}")
@@ -45,7 +52,11 @@ cut_socorro_morphometry() {
 cut_file() {
     data_file=${1}
     number_columns=${2}
-    xlsx_2_csv ${data_file} ${number_columns}
+    xx_xlsx_2_csv ${data_file}
+    file_name=$(get_base_filename ${data_file})
+    tmp_file_name=${file_name}.tmp
+    csv_file_name=${file_name}.csv
+    cut_csv_file ${number_columns} ${tmp_file_name} ${csv_file_name}
 }
     
 cut_csv_file() {
