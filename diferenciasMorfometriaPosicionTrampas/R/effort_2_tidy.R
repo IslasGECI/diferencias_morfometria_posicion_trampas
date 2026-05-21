1df0859a48e12f85790692bbc38728b797b405d7
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-21 18:45:43 +0000

    ♻️  Refactor

--- diferenciasMorfometriaPosicionTrampas/R/effort_2_tidy.R
+++ diferenciasMorfometriaPosicionTrampas/R/effort_2_tidy.R
@@ -5,14 +5,15 @@ effort_2_tidy <- function(datos_prueba) {
     datos_prueba |> dplyr::select(dplyr::all_of(columns_to_select), dplyr::contains("/"))
 
   long_format <- filter_table |>
-    tidyr::pivot_longer(cols = -columns_to_select, names_to = "Fecha", values_to = "Estado_trampa")
+    tidyr::pivot_longer(cols = -dplyr::all_of(columns_to_select), names_to = "Fecha", values_to = "Estado_trampa")
 
   updated_columns_to_select <- c(columns_to_select, "Fecha", "Estado_trampa")
   long_format_selected <- long_format |>
     dplyr::select(dplyr::all_of(updated_columns_to_select)) |>
     dplyr::rename(ID_de_trampa = ID)
 
-  rows_with_captures <- long_format_selected |> filter_captures()
+  rows_with_captures <- long_format_selected |>
+    filter_captures()
   if (nrow(rows_with_captures) == 0) {
     message("NO HAY CAPTURAS")
   }
