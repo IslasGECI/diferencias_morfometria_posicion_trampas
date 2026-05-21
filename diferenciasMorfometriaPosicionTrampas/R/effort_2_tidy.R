c3b12f577ff0738af61d7dd4f5a9bc018262160f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-21 17:18:06 +0000

    ♻️  Refactor

--- diferenciasMorfometriaPosicionTrampas/R/effort_2_tidy.R
+++ diferenciasMorfometriaPosicionTrampas/R/effort_2_tidy.R
@@ -2,7 +2,7 @@
 effort_2_tidy <- function(datos_prueba) {
   columns_to_select <- c("ID", "Atrayente", "Nombre_del_responsable")
   filter_table <-
-    datos_prueba |> dplyr::select(dplyr::any_of(columns_to_select), contains("/"))
+    datos_prueba |> dplyr::select(columns_to_select, contains("/"))
 
   long_format <- filter_table |>
     tidyr::pivot_longer(cols = -columns_to_select, names_to = "Fecha", values_to = "Estado_trampa") |>
