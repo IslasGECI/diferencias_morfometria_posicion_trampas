61cb7d0947b9105adb4318f26c155cd80b2eda02
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-21 18:43:08 +0000

    ✅ Pass tests

--- diferenciasMorfometriaPosicionTrampas/R/effort_2_tidy.R
+++ diferenciasMorfometriaPosicionTrampas/R/effort_2_tidy.R
@@ -5,15 +5,18 @@ effort_2_tidy <- function(datos_prueba) {
     datos_prueba |> dplyr::select(columns_to_select, contains("/"))
 
   long_format <- filter_table |>
-    tidyr::pivot_longer(cols = -columns_to_select, names_to = "Fecha", values_to = "Estado_trampa") |>
-    dplyr::select(ID, Estado_trampa, Fecha, Atrayente, Nombre_del_responsable) |>
+    tidyr::pivot_longer(cols = -columns_to_select, names_to = "Fecha", values_to = "Estado_trampa")
+
+  updated_columns_to_select <- c(columns_to_select, "Fecha", "Estado_trampa")
+  long_format_selected <- long_format |>
+    dplyr::select(updated_columns_to_select) |>
     dplyr::rename(ID_de_trampa = ID)
 
-  rows_with_captures <- long_format |> filter_captures()
+  rows_with_captures <- long_format_selected |> filter_captures()
   if (nrow(rows_with_captures) == 0) {
     message("NO HAY CAPTURAS")
   }
-  return(long_format)
+  return(long_format_selected)
 }
 
 get_existing_columns_to_select_in_df <- function(df) {
