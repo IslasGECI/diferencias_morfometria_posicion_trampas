a43137e1104cbf6b002c5da5387b5aafa0e0eaac
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-21 17:15:05 +0000

    ♻️  Refactor

--- diferenciasMorfometriaPosicionTrampas/R/effort_2_tidy.R
+++ diferenciasMorfometriaPosicionTrampas/R/effort_2_tidy.R
@@ -6,7 +6,8 @@ effort_2_tidy <- function(datos_prueba) {
 
   long_format <- filter_table |>
     tidyr::pivot_longer(cols = -columns_to_select, names_to = "Fecha", values_to = "Estado_trampa") |>
-    dplyr::select(ID_de_trampa = ID, Estado_trampa, Fecha, Atrayente, Nombre_del_responsable)
+    dplyr::select(ID, Estado_trampa, Fecha, Atrayente, Nombre_del_responsable) |>
+    dplyr::rename(ID_de_trampa = ID)
 
   rows_with_captures <- long_format |> filter_captures()
   if (nrow(rows_with_captures) == 0) {
